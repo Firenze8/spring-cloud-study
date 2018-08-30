a04d5938b63609774e25ec75f0293c8358bf13a9
tiantianbaobao <15811425433@163.com>
2018-08-30 17:35:23 +0800

    新增test package

---
+++ spring-cloud-study/spring-cloud-stream/src/main/java/com/csii/springcloudstream/test/MyTest.java
@@ -0,0 +1,11 @@
+package com.csii.springcloudstream.test;
+
+/**
+ * @author cuitao
+ * @version V1.0
+ * @Description: ${todo}
+ * @date 2018/8/30 - 17:24 -- 星期四
+ */
+public class MyTest {
+    //
+}
